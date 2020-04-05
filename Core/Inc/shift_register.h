f03b0d2b908f1cbc3cbe8e39d7689a95376b9b70
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-04-04 20:30:42 -0700

    Update IDE and IOC

---
+++ Core/Inc/shift_register.h
@@ -0,0 +1,13 @@
+/*
+ * shift_register.h
+ *
+ *  Created on: Apr. 4, 2020
+ *      Author: Konstantin
+ */
+
+#ifndef INC_SHIFT_REGISTER_H_
+#define INC_SHIFT_REGISTER_H_
+
+
+
+#endif /* INC_SHIFT_REGISTER_H_ */
