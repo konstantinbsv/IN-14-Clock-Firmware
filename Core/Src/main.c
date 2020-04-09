52efa88695f209b179989322a120a9e960b2d785
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-04-08 19:44:39 -0700

    Include RTC in main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,6 +24,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "shift_register.h"
+#include "rtc.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
