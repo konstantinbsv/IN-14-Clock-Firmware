5c2f8c6f2bac5972ddddc31d309a284e562b5f40
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-04-08 19:38:28 -0700

    Create library for RTC

---
+++ Core/Inc/rtc.h
@@ -0,0 +1,13 @@
+/*
+ * rtc.h
+ *
+ *  Created on: Apr 8, 2020
+ *      Author: Konstantin Borissov
+ */
+
+#ifndef INC_RTC_H_
+#define INC_RTC_H_
+
+
+
+#endif /* INC_RTC_H_ */
---
+++ Core/Src/rtc.c
@@ -0,0 +1,8 @@
+/*
+ * rtc.h
+ *
+ *  Created on: Apr 8, 2020
+ *      Author: Konstantin Borissov
+ */
+
+
