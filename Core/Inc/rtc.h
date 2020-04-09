79ea773c0d78b2c7a35b96b22833b22d710c3080
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-04-08 20:08:29 -0700

    Create functions for setting time and date

--- Core/Inc/rtc.h
+++ Core/Inc/rtc.h
@@ -13,6 +13,7 @@
 
 /* Function Prototypes */
 HAL_StatusTypeDef InitializeRTC (RTC_HandleTypeDef *_rtc_handle);
-
+HAL_StatusTypeDef SetTime(uint8_t hours, uint8_t minutes, uint8_t seconds);
+HAL_StatusTypeDef SetDate(	uint8_t week_day, uint8_t month, uint8_t date, uint8_t year);
 
 #endif /* INC_RTC_H_ */
--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -16,3 +16,43 @@ HAL_StatusTypeDef InitializeRTC (RTC_HandleTypeDef *_rtc_handle) {
 	return rtc_handle != NULL ? HAL_OK : HAL_ERROR;
 }
 
+/**
+ * Set the time
+ */
+HAL_StatusTypeDef SetTime(uint8_t hours, uint8_t minutes, uint8_t seconds) {
+	assert_param(hours <= 23U);
+	assert_param(hours <= 59U);
+	assert_param(seconds <= 59U);
+
+	RTC_TimeTypeDef sTime = {0};
+
+	sTime.Hours = hours;
+	sTime.Minutes = minutes;
+	sTime.Seconds = seconds;
+
+	return HAL_RTC_SetTime(rtc_handle, &sTime, RTC_FORMAT_BCD);
+}
+
+/**
+ * Set the date
+ */
+HAL_StatusTypeDef SetDate(	uint8_t week_day,
+							uint8_t month,
+							uint8_t date,
+							uint8_t year)
+{
+	assert_param(week_day >= RTC_WEEKDAY_SUNDAY && week_day <= RTC_WEEKDAY_SATURDAY);
+	assert_param(month >= RTC_MONTH_JANUARY && month <= RTC_MONTH_DECEMBER);
+	assert_param(date >= 1 && date <= 31);
+	assert_param(year <= 99);
+
+	RTC_DateTypeDef DateToUpdate = {0};
+
+	DateToUpdate.WeekDay = week_day;
+	DateToUpdate.Month = month;
+	DateToUpdate.Date = date;
+	DateToUpdate.Year = year;
+
+	return HAL_RTC_SetDate(rtc_handle, &DateToUpdate, RTC_FORMAT_BCD);
+
+}
